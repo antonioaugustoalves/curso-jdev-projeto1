4d99ac7a9cb828462493e1fb6e04cf744b761f0b
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2022-07-27 12:08:10 -0300

    Metodos com retorno

--- src/br/com/antonioaugusto/classes/Sistema.java
+++ src/br/com/antonioaugusto/classes/Sistema.java
@@ -12,9 +12,19 @@ public class Sistema {
 		System.out.println(aluno2.getNome());
 		System.out.println(aluno3.getNome());
 		
+		aluno1.setNota1(100);
+		aluno1.setNota2(95);
+		aluno1.setNota3(100);
+		aluno1.setNota4(80);
+		
+		String resultado = aluno1.getAprovado();
+		System.out.println(resultado);
+		System.out.println(aluno1.getRsultadoFinal()?"Aprovado":"Reprovado");
+		
 		
 			
 
+		
 	}
 
 }
